27584ac446b736d228cc2c51f9ee4ad3e9f19c33
kendricksharpe <kendricksharpe@berkeley.edu>
2022-03-10 13:46:12 -0800

    testing

--- js/behavior.js
+++ js/behavior.js
@@ -2,6 +2,7 @@ function nextMeme(){
     //Modify the src attribute of the image with the ID "myImage"
     let n = Array("images/memes/1.jpg", "images/memes/2.jpg", "images/memes/3.jpg")
     let b = document.getElementById("meme").src
+    console.log(b)
     var a = [];
     n.forEach(function(p){
         if(!(p === b)){
