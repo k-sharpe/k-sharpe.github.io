527c7f712d1c545a798049ad4ae7f0127be73e59
kendricksharpe <kendricksharpe@berkeley.edu>
2022-03-10 10:48:46 -0800

    fix behavior

--- js/behavior.js
+++ js/behavior.js
@@ -1,6 +1,13 @@
 function nextMeme(){
     //Modify the src attribute of the image with the ID "myImage"
-    let a = Array("images/memes/1.jpg", "images/memes/2.jpg", "images/memes/3.jpg")
+    let n = Array("images/memes/1.jpg", "images/memes/2.jpg", "images/memes/3.jpg")
+    let b = document.getElementById("meme").src
+    var a = [];
+    n.forEach(function(p){
+        if(!(p === b)){
+            a.push(p);
+        }
+    });
     let randomElemt = a[Math.floor(Math.random()*a.length)]
     document.getElementById("meme").src = randomElemt;
 }
