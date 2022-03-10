406360ea27e1137c2455842741e3d3e812d9a3dc
kendricksharpe <kendricksharpe@berkeley.edu>
2022-03-10 10:38:11 -0800

    fix memes

--- js/behavior.js
+++ js/behavior.js
@@ -1,6 +1,6 @@
 function nextMeme(){
     //Modify the src attribute of the image with the ID "myImage"
-    let list = Array("images/memes/1.jpg", "images/memes/2.jpg", "images/memes/3.jpg")
-    let randomElemt = song[list.floor(Math.random()*list.length)]
+    let a = Array("images/memes/1.jpg", "images/memes/2.jpg", "images/memes/3.jpg")
+    let randomElemt = a[Math.floor(Math.random()*a.length)]
     document.getElementById("meme").src = randomElemt;
 }
